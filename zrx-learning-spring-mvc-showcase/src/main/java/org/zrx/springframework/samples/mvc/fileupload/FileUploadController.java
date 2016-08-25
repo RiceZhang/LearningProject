ab17002bbed728a5f81c5113ee42c71e41cbd51d
apple <apple.zhang@yolipai.com>
2016-08-25 09:21:36 +0800

    aa Signed-off-by: apple <apple.zhang@yolipai.com>

--- zrx-learning-spring-mvc-showcase/src/main/java/org/zrx/springframework/samples/mvc/fileupload/FileUploadController.java
+++ zrx-learning-spring-mvc-showcase/src/main/java/org/zrx/springframework/samples/mvc/fileupload/FileUploadController.java
@@ -1,9 +1,26 @@
 package org.zrx.springframework.samples.mvc.fileupload;
 
+        import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.ModelAttribute;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.context.request.WebRequest;
+        import org.zrx.springframework.mvc.extensions.ajax.AjaxUtils;
+
 /**
  * Function:    FileUploadController
  * Author:      zhangrixiong
  * DateTime:    2016/8/22 17:53
  */
+
+@Controller
+@RequestMapping("/fileupload")
 public class FileUploadController {
+
+    @ModelAttribute
+    public void ajaxAttribute(WebRequest request, Model model) {
+        model.addAttribute("ajaxRequest", AjaxUtils.isAjaxRequest(request));
+    }
+
+
 }
