544f524addd36c0dc3ee3f390c0d67bbcd9dd0d4
apple <apple.zhang@yolipai.com>
2016-09-01 13:40:16 +0800

    aa
    Signed-off-by: apple <apple.zhang@yolipai.com>

--- zrx-learning-spring-mvc-showcase/src/main/java/org/zrx/springframework/samples/mvc/convert/MaskFormat.java
+++ zrx-learning-spring-mvc-showcase/src/main/java/org/zrx/springframework/samples/mvc/convert/MaskFormat.java
@@ -9,7 +9,7 @@ import java.lang.annotation.*;
  * DateTime:    2016/8/22 17:43
  */
 @Target(value={ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RetentionPolicy.RUNTIME)   // 运行阶段，以为着可以采用反射
 @Documented
 public @interface MaskFormat {
     String value();
--- zrx-learning-spring-mvc-showcase/src/main/java/org/zrx/springframework/samples/mvc/mapping/MappingController.java
+++ zrx-learning-spring-mvc-showcase/src/main/java/org/zrx/springframework/samples/mvc/mapping/MappingController.java
@@ -2,7 +2,10 @@ package org.zrx.springframework.samples.mvc.mapping;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Function:    MappingController
@@ -11,10 +14,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class MappingController {
+
     @RequestMapping("/mapping/path")
     public @ResponseBody String byPath() {
         return "Mapped by path!";
     }
 
+    @RequestMapping(value="/mapping/path/*", method = RequestMethod.GET)
+    public @ResponseBody String byPathPattern(HttpServletRequest request) {
+        return "Mappped by path pattern( " + request.getRequestURI() + ")";
+    }
 
+    @RequestMapping(value = "/mapping/method", method=RequestMethod.GET)
+    public @ResponseBody String byMethod() {
+        return "Mapped by path + method";
+    }
+
+    @RequestMapping(value = "/mapping/parameter", method = RequestMethod.GET, params = "foo" )
+    public @ResponseBody String  byParameter() {
+        return "Mapped by path + method + presence of query parameter! ";
+    }
+
+    public  @ResponseBody String byParameterNegation() {
+        return "Mapped by path ";
+    }
 }
